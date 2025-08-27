3ff5128ca799c42a5dc0adeef4416693ce200e32
Andrii Maiskyi <andrey@wearehelful.com>
2025-08-27 18:25:59 +0100

    feat: waitlist. #1

--- src/schema/lists/Waitlist/Waitlist.ts
+++ src/schema/lists/Waitlist/Waitlist.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Lists } from ".keystone/types";
 import { list } from "@keystone-6/core";
-import { text } from "@keystone-6/core/fields";
+import { text, timestamp } from "@keystone-6/core/fields";
 import { allowAll } from "@keystone-6/core/access";
 import { Session } from "../lists.types";
 
@@ -12,6 +12,19 @@ export const Waitlist = list<Lists.Config.TypeInfo<Session>>({
       isIndexed: "unique",
       validation: {
         isRequired: true,
+      },
+    }),
+    createdAt: timestamp({
+      ui: {
+        createView: {
+          fieldMode: "hidden",
+        },
+        itemView: {
+          fieldMode: "read",
+        },
+      },
+      defaultValue: {
+        kind: "now",
       },
     }),
   },
